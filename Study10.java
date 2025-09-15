d74faca7f4ca9ef0d7315335c571f73fbaf34217
lee <tmddl08@naver.com>
2025-09-15 16:37:28 +0900

    Add files via upload

--- Study10.java
+++ Study10.java
@@ -1,9 +1,22 @@
 package variable1;
 
+import java.util.Random;
+
 public class Study10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 주사위를 두번 던졌을 때 각각 결과를 출력하고 두 숫자의 합을 출력
+		//random : 
+		
+		Random ran = new Random();
+		int num1 = ran.nextInt(6) +1;
+				System.out.println("첫번째 주사위 랜덤 숫자:" + num1);
+				
+		int num2 = ran.nextInt(6) +1;
+				System.out.println("두번째 주사위 랜덤 숫자:" + num2);
+		int sum = num1 + num2;
+				System.out.println("두 숫자의 합:" + sum);
+				//"두 숫자의 합:" + (num1+num2));
 
 	}
 
--- Study11.java
+++ Study11.java
@@ -1,10 +1,39 @@
 package variable1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Study11 {
 
 	public static void main(String[] args) {
 		// 1~50 중 랜덤 숫자를 하나 만들고, 사용자가 숫자를 입력해서 맞출 때까지 "UP" 또는 "DOWN"을 출력
-
+	Random ran = new Random();
+	int num = ran.nextInt(50)+1;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("숫자를 입력해주세요");
+	
+	while (true) { // 계속반복
+		int i= sc.nextInt();
+		
+		if (i< num) {
+			System.out.println("up");
+		}else if (i>num ) {
+			System.out.println("down");
+		}else {
+			System.out.println("정답입니다."); break; //반복문 종료
+			
+			
+			
+			
+			
+		}
+		
+	}
+		
+		
+		
+		
+		
 	}
 
 }
